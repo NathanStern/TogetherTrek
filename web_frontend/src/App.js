efeabfedd9d6fb46628a2d289325bd2944b62c11
Adil <aabdugal@gmail.com>
2021-02-23 17:33:46 -0500

    started working on header

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -7,7 +8,9 @@ function App() {
     <>
       <Header />
       <main>
-        <h1>TogetherTrek</h1>
+        <Container>
+          <h1>BODY</h1>
+        </Container>
       </main>
       <Footer />
     </>
--- web_frontend/src/components/Header.js
+++ web_frontend/src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
     return (
         <header>
-            header
+            <Navbar bg="primary" expand="lg">
+                <Container>
+                    <Navbar.Brand href="./">TogetherTrek</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="./login">Login</Nav.Link>
+                            <Nav.Link href="./register">Register</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
