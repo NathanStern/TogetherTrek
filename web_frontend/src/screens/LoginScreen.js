47668f9c8de0b3b2fa58b9d6fd2735c0ad707b9f
Adil <aabdugal@gmail.com>
2021-02-25 09:38:59 -0500

    Added details to login and register pages

--- web_frontend/src/screens/LoginScreen.js
+++ web_frontend/src/screens/LoginScreen.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        console.log(`Email is ${email} and password is ${password}`)
+    }
     return (
         <div>
-            Login Screen
+            <h3 className='col-lg-4'> Sign In</h3>
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label> Email:  </label>
+                    <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+                </div>
+                <div className='form-group'>
+                    <label> Password:</label>
+                    <input type="text" value={password} onChange={(event) => setPassword(event.target.value)} />
+                </div>
+                <button >Submit</button>
+            </form>
         </div>
     )
 }
--- web_frontend/src/screens/RegisterScreen.js
+++ web_frontend/src/screens/RegisterScreen.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 const RegisterScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [repeatedPassword, setRepeatedPassword] = useState('')
+    const handleSubmit = () => {
+        console.log(`Email is ${email} and password is ${password} : ${repeatedPassword}`)
+    }
     return (
         <div>
-            Register screen
+            <h3 className='col-lg-4'> Register</h3>
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label> Email:  </label>
+                    <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+                </div>
+                <div className='form-group'>
+                    <label> Password:</label>
+                    <input type="text" value={password} onChange={(event) => setPassword(event.target.value)} />
+                </div>
+                <div className='form-group'>
+                    <label> Repeat Password:</label>
+                    <input type="text" value={repeatedPassword} onChange={(event) => setRepeatedPassword(event.target.value)} />
+                </div>
+                <button >Submit</button>
+            </form>
         </div>
     )
 }
