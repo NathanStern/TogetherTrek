cc1d4b095cd360be98b19fc25e7c582c005141c3
Nathan Stern <sternn@purdue.edu>
2021-02-28 14:29:28 -0500

    added barebones location class

--- mobile_frontend/TogetherTrek/lib/models/LocationModel.dart
+++ mobile_frontend/TogetherTrek/lib/models/LocationModel.dart
@@ -1,1 +1,8 @@
-class LocationModel {}
+import 'dart:ffi';
+
+class LocationModel {
+  List<Float> coordinates;
+  LocationModel(List<Float> coordinates) {
+    this.coordinates = coordinates;
+  }
+}
