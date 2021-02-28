573e65416677a745ebc72f233ab902bd36d0f97c
Nathan Stern <sternn@purdue.edu>
2021-02-28 14:30:39 -0500

    added barebones profile pic class

--- mobile_frontend/TogetherTrek/lib/models/ProfilePicModel.dart
+++ mobile_frontend/TogetherTrek/lib/models/ProfilePicModel.dart
@@ -1,1 +1,9 @@
-class ProfilePicModel {}
+class ProfilePicModel {
+  String url;
+  String uploadDate;
+
+  ProfilePicModel(String url, String uploadDate) {
+    this.url = url;
+    this.uploadDate = uploadDate;
+  }
+}
