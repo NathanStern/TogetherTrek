c45429c8abf072a28623a90abcba9a0e6c3237b1
Nathan Stern <sternn@purdue.edu>
2021-02-28 18:13:33 -0500

    added trip model and associated classes

---
+++ mobile_frontend/TogetherTrek/lib/models/TripModel.dart
@@ -0,0 +1,45 @@
+import 'package:TogetherTrek/models/TripPhotoModel.dart';
+import 'package:flutter/material.dart';
+
+class TripModel extends ChangeNotifier {
+  String id;
+  String destination;
+  String startDate;
+  String endDate;
+  String creatorId;
+  List<String> participantIds;
+  List<TripPhotoModel> tripPhotos;
+
+  TripModel(
+      String id,
+      String destination,
+      String startDate,
+      String endDate,
+      String creatorId,
+      List<String> participantIds,
+      List<TripPhotoModel> tripPhotos) {
+    this.id = id;
+    this.destination = destination;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.creatorId = creatorId;
+    this.participantIds = participantIds;
+    this.tripPhotos = tripPhotos;
+
+    notifyListeners();
+  }
+
+  void setStartDate(String newDate) {
+    this.startDate = newDate;
+    notifyListeners();
+  }
+
+  void setEndDate(String newDate) {
+    this.endDate = newDate;
+    notifyListeners();
+  }
+
+  void addParticipant(String participantId) {
+    this.participantIds.add(participantId);
+  }
+}
---
+++ mobile_frontend/TogetherTrek/lib/models/TripPhotoModel.dart
@@ -0,0 +1,9 @@
+class TripPhotoModel {
+  String url;
+  String postDate;
+
+  TripPhotoModel(String url, String postDate) {
+    this.url = url;
+    this.postDate = postDate;
+  }
+}
