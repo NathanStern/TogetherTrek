1a623d19566c7bfaadadc7fe95bf9d8c1b0a90ce
Nathan Stern <sternn@purdue.edu>
2021-02-28 19:57:48 -0500

    removed unused packages and fixed coding style

--- mobile_frontend/TogetherTrek/lib/models/ContentModel.dart
+++ mobile_frontend/TogetherTrek/lib/models/ContentModel.dart
@@ -4,16 +4,16 @@ class ContentModel {
   String postDate;
   String type; // text or image
   String data; // text or url to image
-  String collection_id;
+  String collectionId;
 
   ContentModel(String id, String authorId, String postDate, String type,
-      String data, String collection_id) {
+      String data, String collectionId) {
     this.id = id;
     this.authorId = authorId;
     this.postDate = postDate;
     this.type = type;
     this.data = data;
-    this.collection_id = collection_id;
+    this.collectionId = collectionId;
   }
 
   // getters are implicit
