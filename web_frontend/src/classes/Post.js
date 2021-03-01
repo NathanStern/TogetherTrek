4a904e2e8ba9309fdef5c7a1e715baf85ff323ec
Isabella Stookey <istookey@purdue.edu>
2021-02-28 20:55:27 -0500

    added dates to post class

--- web_frontend/src/classes/Post.js
+++ web_frontend/src/classes/Post.js
@@ -3,13 +3,15 @@ This is the file for the Post class which holds all of the data related to a
 post and functions related to posts
 */
 
-export function Post(id, authorId, title, description, postDate, destinations) {
+export function Post(id, authorId, title, description, postDate, destinations, tripStartDate, tripEndDate) {
   this.id = id;
   this.authorId = authorId;
   this.title = title;
   this.description = description;
   this.postDate = postDate;
   this.destinations = destinations;
+  this.tripStartDate = tripStartDate;
+  this.tripEndDate = tripEndDate;
 
   // field getters are implicit i.e. just use post.id
 
@@ -22,5 +24,11 @@ export function Post(id, authorId, title, description, postDate, destinations) {
   }
   this.setDetinations = function(newValue) {
     this.destinations = newValue;
+  }
+  this.setTripStartDate = function(newValue) {
+    this.tripStartDate = newValue;
+  }
+  this.setTripEndDate = function(newValue) {
+    this.tripEndDate = newValue;
   }
 }
