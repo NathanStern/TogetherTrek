f4157525c5e65894d243f6b2f8ff7f939a72930e
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2021-02-28 20:54:30 -0500

    Added blank profile photo to front end

---
+++ web_frontend/src/screens/EditProfileScreen.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+const logo = {
+    uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    width: 64,
+    height: 64
+};
+const SearchScreen = () => {
+    return (
+        <div>
+            will contain editing profile features
+        </div>
+    )
+}
+
+export default SearchScreen
--- web_frontend/src/screens/ProfileScreen.js
+++ web_frontend/src/screens/ProfileScreen.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react'
+const logo = {
+    uri: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    width: 64,
+    height: 64
+};
 const ProfileScreen = () => {
-    return (<div>
-        
-    </div>)
+    return (
+    <div>
+        Will contain profile info
+    </div>
+    )
 }
 export default ProfileScreen
