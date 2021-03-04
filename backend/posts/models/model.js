34e71aaf2fe42817fc8b291e5d8053cf5ad2b1de
Brian Yang <yang1839@purdue.edu>
2021-03-04 07:07:42 -0500

    update posts model

--- backend/posts/models/model.js
+++ backend/posts/models/model.js
@@ -1,15 +1,6 @@
 module.exports = mongoose => {
-    const Tutorial = mongoose.model(
-      "tutorial",
-      mongoose.Schema(
-        {
-          title: String,
-          description: String,
-          published: Boolean
-        },
-        { timestamps: true }
-      )
-    );
-  
-    return Tutorial;
-  };
+  const Post = mongoose.model(
+      //Post structure
+  );
+  return Post;
+}
