016a5b220c7940f7c64afcd83ca3cec364020c78
Brian Yang <yang1839@purdue.edu>
2021-03-04 06:25:16 -0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,29 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./posts/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome TogetherTrek." });
+  res.json({ message: "Welcome to TogetherTrek." });
 });
 
+require("./posts/routes/routes.js")(app);
+
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
