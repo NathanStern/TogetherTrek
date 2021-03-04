5ee29d53fd1770dd014990c7eb21fe84c53fb734
Brian Yang <yang1839@purdue.edu>
2021-03-04 06:55:26 -0500

    update welcome message

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to TogetherTrek." });
+  res.json("Welcome to TogetherTrek.");
 });
 
 require("./posts/routes/routes.js")(app);
