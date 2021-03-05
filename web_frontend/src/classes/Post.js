a39156857d7350bee641e597ebf07f941f699234
Isabella Stookey <istookey@purdue.edu>
2021-03-05 17:41:54 -0500

    reverted changes to Post.js

--- web_frontend/src/classes/Post.js
+++ web_frontend/src/classes/Post.js
@@ -3,15 +3,13 @@ This is the file for the Post class which holds all of the data related to a
 post and functions related to posts
 */
 
-export function Post(id, authorId, title, description, postDate, destinations, tripStartDate, tripEndDate) {
+export function Post(id, authorId, title, description, postDate, destinations) {
   this.id = id;
   this.authorId = authorId;
   this.title = title;
   this.description = description;
   this.postDate = postDate;
   this.destinations = destinations;
-  this.tripStartDate = tripStartDate;
-  this.tripEndDate = tripEndDate;
 
   // field getters are implicit i.e. just use post.id
 
@@ -24,11 +22,5 @@ export function Post(id, authorId, title, description, postDate, destinations, t
   }
   this.setDetinations = function(newValue) {
     this.destinations = newValue;
-  }
-  this.setTripStartDate = function(newValue) {
-    this.tripStartDate = newValue;
-  }
-  this.setTripEndDate = function(newValue) {
-    this.tripEndDate = newValue;
   }
 }
