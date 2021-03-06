355f37203392190519e29ebe097806990afe0dc4
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2021-03-05 20:43:39 -0500

    Updated schema in model

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -2,9 +2,29 @@ module.exports = mongoose => {
   var schema = mongoose.Schema(
     /* temporary schema we'll need to fill in our own schema */
     {
-      title: String,
-      description: String,
-      published: Boolean
+      id: Number,
+      username: String,
+	    password: String,
+	    email: String,
+	    birthdate: String,
+	    gender: String,
+	    first_name: String,
+	    last_name: String,
+	    profile_pic: {
+		    upload_date: String,
+		    link: String
+	    },
+	    verified: Boolean,
+	    notifications_enabled: Boolean,
+	    location_enabled: Boolean,
+	    location: {
+		    type: String,
+		    coordinates: [Number]
+	    },
+	    post_ids: [Number],
+	    trip_ids: [Number],
+	    message_board_ids: [Number],
+	    friend_ids: [Number]
     },
     { timestamps: true}
   );
