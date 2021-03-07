317bcc3dfcd9759f0cd7dd700c8af888453624b9
Nathan Stern <sternn@purdue.edu>
2021-03-06 23:02:22 -0500

    Changed the application title

--- mobile_frontend/together_trek/lib/main.dart
+++ mobile_frontend/together_trek/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TogetherTrek',
       theme: ThemeData(
         // This is the theme of your application.
         //
