e52ab127a352f67909a2d3eaff1463d99124b8f7
Nathan Stern <sternn@purdue.edu>
2021-03-07 00:46:43 -0500

    added skeleton for getUser() method

--- mobile_frontend/together_trek/lib/api/UserWrapper.dart
+++ mobile_frontend/together_trek/lib/api/UserWrapper.dart
@@ -1,6 +1,10 @@
 import "package:together_trek/api/httpRequest.dart";
 import 'package:together_trek/models/UserModel.dart';
 
+/**
+ * This method gets a user with a specific id from the
+ * database and returns it to the caller
+ */
 UserModel getUser(String id) {
   // implement an http request call
 }
