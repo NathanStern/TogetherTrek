d2945888751aba1b8bd1475ab113c19b565516d2
Nathan Stern <sternn@purdue.edu>
2021-03-06 22:19:41 -0500

    Learned how to use Flexible widget

--- mobile_frontend/together_trek/lib/views/PlaceholderView.dart
+++ mobile_frontend/together_trek/lib/views/PlaceholderView.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class PlaceholderView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var c_width = MediaQuery.of(context).size.width * 0.8;
     return Scaffold(
         appBar: AppBar(
           title: Text("TogetherTrek: Unfinished Route"),
@@ -11,8 +12,11 @@ class PlaceholderView extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text("This page has not been implemented yet",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+              Container(
+                  child: Flexible(
+                      child: Text("This page has not been implemented yet",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold))))
             ])
           ],
         ));
