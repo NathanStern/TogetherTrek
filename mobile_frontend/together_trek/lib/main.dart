669a4b4c28454f681d20bfc499f466c52c5bc284
Nathan Stern <sternn@purdue.edu>
2021-03-07 01:09:30 -0500

    added a drawer to the main page

--- mobile_frontend/together_trek/lib/main.dart
+++ mobile_frontend/together_trek/lib/main.dart
@@ -98,15 +98,28 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text("PLACEHOLDER HEADER"),
+            decoration: BoxDecoration(color: Colors.deepOrange),
+          ),
+          ListTile(
+              title: Text("Profile"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()));
+              }),
+          ListTile(title: Text("Settings")),
+        ],
+      )),
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        leading: IconButton(
-          tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
-        ),
         actions: [
           PopupMenuButton(
             itemBuilder: (context) {
