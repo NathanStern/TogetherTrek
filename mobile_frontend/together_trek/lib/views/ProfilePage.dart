435d4c66840477045211b7c0cc9f4680e80e643c
Brian Yang <yang1839@purdue.edu>
2021-03-11 14:06:13 -0500

    profile page with sample user

--- mobile_frontend/together_trek/lib/views/ProfilePage.dart
+++ mobile_frontend/together_trek/lib/views/ProfilePage.dart
@@ -7,17 +7,213 @@ import 'package:together_trek/views/ProfileInfoView.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(title: Text("Profile")),
-        body: Container(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Center(
-        child: Consumer<UserModel>(
-            builder: (context, user, child) => Container(
-                child: Column(children: [ProfileInfoView(user: user)]))),
-      )
-    ])));
+      body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.redAccent, Colors.orangeAccent]
+              )
+            ),
+            child: Container(
+              width: double.infinity,
+              height: 320.0,
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      
+                      backgroundImage: NetworkImage(
+                        "https://www.biography.com/.image/t_share/MTIwNjA4NjMzNDA3NTcxNDY4/neil-armstrong-9188943-2-402.jpg",
+                      ),
+                      radius: 50.0,
+                      
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Neil Armstrong",
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Card(
+                      margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
+                      clipBehavior: Clip.antiAlias,
+                      color: Colors.white,
+                      elevation: 5.0,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 22.0),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Column(
+
+                                children: <Widget>[
+                                  Text(
+                                    "Traveling to",
+                                    style: TextStyle(
+                                      color: Colors.redAccent,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    "The Moon",
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.orangeAccent,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+
+                                children: <Widget>[
+                                  Text(
+                                    "Budget",
+                                    style: TextStyle(
+                                      color: Colors.redAccent,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    "25.4B",
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.orangeAccent,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+
+                                children: <Widget>[
+                                  Text(
+                                    "Date",
+                                    style: TextStyle(
+                                      color: Colors.redAccent,
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    "July/16/1969",
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.orangeAccent,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0,horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                      "Bio:",
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 28.0
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text('One small step for man, one giant leap for mankind.',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            height: 330,
+            
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            width: 300.00,
+
+            child: RaisedButton(
+              onPressed: (){},
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(80.0)
+              ),
+              elevation: 0.0,
+                padding: EdgeInsets.all(0.0),
+              child: Ink(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerRight,
+                    end: Alignment.centerLeft,
+                    colors: [Colors.redAccent,Colors.orangeAccent]
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                  alignment: Alignment.center,
+                  child: Text("Contact me",
+                  style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300),
+                  ),
+                ),
+              )
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
