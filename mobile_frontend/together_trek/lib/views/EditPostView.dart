d28a96520001f27204c35f852ec68de03c3aece3
Isabella Stookey <53879198+istookey@users.noreply.github.com>
2021-03-12 03:16:16 -0500

    Update mobile_frontend/together_trek/lib/views/EditPostView.dart
    Co-authored-by: NathanStern <sternn@purdue.edu>

--- mobile_frontend/together_trek/lib/views/EditPostView.dart
+++ mobile_frontend/together_trek/lib/views/EditPostView.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:together_trek/models/PostModel.dart';
 
 import 'package:together_trek/api/PostWrapper.dart';
