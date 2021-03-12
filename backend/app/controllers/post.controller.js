180ae595eb0dd5ea04b4f394be0120c31bbcdea2
Isabella Stookey <istookey@purdue.edu>
2021-03-12 02:47:13 -0500

    make and edit post on mobile

--- backend/app/controllers/post.controller.js
+++ backend/app/controllers/post.controller.js
@@ -91,7 +91,28 @@ exports.create = (req, res) => {
   
   // Updates an entry in the posts table by id
   exports.update = (req, res) => {
+    if (!req.body) {
+      return res.status(400).send({
+        message: "Cannot update Post with empty data"
+      })
+    }
   
+    const id = req.params.id;
+  
+    User.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+      .then(data => {
+        if (!data) {
+          res.status(404).send({message: `Could not find Post with id=${id}.`});
+        }
+        else {
+          res.send({message: "Post was updated successfully!"});
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({message: `Error retrieving Post with id=${id}.`});
+      });
   }
   
   // Deletes an entry in the posts table by id
