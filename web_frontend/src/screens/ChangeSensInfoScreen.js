840bfe740c137038076ec10698c8d28bf79901ae
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2021-03-31 11:18:09 -0400

    changeSensInfo screen works

--- web_frontend/src/screens/ChangeSensInfoScreen.js
+++ web_frontend/src/screens/ChangeSensInfoScreen.js
@@ -1,4 +1,4 @@
-import '../index.css';
+import '../index.css'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
@@ -7,93 +7,138 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { updateUserProfile } from '../actions/userActions'
-import {path} from '../constants/pathConstant'
+import { path } from '../constants/pathConstant'
 import axios from 'axios'
 import { sha3_256 } from 'js-sha3'
 
 const logo = {
-    uri:
-      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
-    width: 64,
-    height: 64,
-  }
-  const ChangeSensInfoScreen = ({ location, history }) => {
-    const { userInfo } = useSelector((state) => state.userLogin)
-  
-    const redirect = '/'
-  
-    useEffect(() => {
-      if (!userInfo) {
-        history.push(redirect)
-      }
-    }, [history, userInfo, redirect])
-    const [username, setUsername] = useState(userInfo.username)
-    const [firstName, setFirstName] = useState(userInfo.first_name)
-    const [lastName, setLastName] = useState(userInfo.last_name)
-    const [email, setEmail] = useState(userInfo.email)
-    const [password, setPassword] = useState(userInfo.password)
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [message, setMessage] = useState(null)
-    const [gender, setGender] = useState(userInfo.gender)
-    const [birthdate, setBirthdate] = useState(userInfo.birthdate)
-    const dispatch = useDispatch()
-  
-    const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
-    const { loading, success, error } = userUpdateProfile
-    const submitHandler = async (e) => {
-      e.preventDefault()
-      const hashedPassword = sha3_256(password)
-      const { data } = await axios.put(`${path}/user/${userInfo._id}`, {...userInfo, password: hashedPassword,})
-    }
-    return (
-    <FormContainer>
-        <h2>Changing sensitive info</h2>
-        {message && <Message variant='danger'>{message}</Message>}
-        {error && <Message variant='danger'>{error}</Message>}
-        {success && <Message variant='success'>Profile Updated</Message>}
-        {loading && <Loader />}
-        <h4>Change password</h4>
-        <Form.Group controlId='confirmPassword'>
-          <Form.Label>Current Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Current Password'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId='confirmPassword'>
-          <Form.Label>New Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='New Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Button type='submit' variant='primary'>
-          Update
-        </Button>
+  uri:
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  width: 64,
+  height: 64,
+}
+const ChangeSensInfoScreen = ({ location, history }) => {
+  const { userInfo } = useSelector((state) => state.userLogin)
 
-        <h4>Change Email</h4>
-        <Form.Group controlId='confirmPassword'>
-          <Form.Label>New Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='New Password'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(sha3_256(e.target.value))}
-          ></Form.Control>
-        </Form.Group>
-        <Button type='submit' variant='primary'>
-          Update
-        </Button>
-        <Row>
-          <Col>
-            <Link to={'/editprofile'}>Back</Link>
-          </Col>
-        </Row>
+  const redirect = '/'
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push(redirect)
+    }
+  }, [history, userInfo, redirect])
+  const [message, setMessage] = useState('')
+  const [email, setEmail] = useState(userInfo.email)
+  const [success, setSuccess] = useState('')
+  const [confirmCurrentPassword, setConfirmCurrentPassword] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const dispatch = useDispatch()
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    if (sha3_256(confirmCurrentPassword) !== userInfo.password) {
+      setMessage('Old password is different')
+    } else {
+      const hashedPassword = sha3_256(password)
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: userInfo.token,
+        },
+      }
+      const { data } = await axios.put(
+        `${path}/users/${userInfo._id}`,
+        {
+          ...userInfo,
+          password: hashedPassword,
+        },
+        config
+      )
+      console.log(data)
+      setSuccess('Password successfully changed')
+    }
+  }
+  const submitEmailHandler = async (e) => {
+    e.preventDefault()
+    if (sha3_256(confirmCurrentPassword) !== userInfo.password) {
+      setMessage('Old password is different')
+    } else {
+      const newEmail = email
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: userInfo.token,
+        },
+      }
+      const { data } = await axios.put(
+        `${path}/users/${userInfo._id}`,
+        {
+          ...userInfo,
+          email: newEmail,
+        },
+        config
+      )
+      console.log(data)
+      setSuccess('Password successfully changed')
+    }
+  }
+  return (
+    <FormContainer>
+      <h2>Changing sensitive info</h2>
+      <h4>Change password</h4>
+      {message && <Message variant='danger'>{message}</Message>}
+      {success && <Message variant='success'>{success}</Message>}
+
+      <Form.Group controlId='confirmPassword'>
+        <Form.Label>Current Password</Form.Label>
+        <Form.Control
+          type='password'
+          placeholder='Current Password'
+          value={confirmCurrentPassword}
+          onChange={(e) => setConfirmCurrentPassword(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group controlId='confirmPassword'>
+        <Form.Label>New Password</Form.Label>
+        <Form.Control
+          type='password'
+          placeholder='New Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+      <Button type='submit' variant='primary' onClick={submitHandler}>
+        Update
+      </Button>
+
+      <h4>Change Email</h4>
+      <Form.Group controlId='email'>
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type='email'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail()}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group controlId='confirmPassword'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type='password'
+          placeholder='Password'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword()}
+        ></Form.Control>
+      </Form.Group>
+      <Button type='submit' variant='primary' onClick={submitEmailHandler}>
+        Update
+      </Button>
+      <Row>
+        <Col>
+          <Link to={'/editprofile'}>Back</Link>
+        </Col>
+      </Row>
     </FormContainer>
-    );
+  )
 }
 export default ChangeSensInfoScreen
