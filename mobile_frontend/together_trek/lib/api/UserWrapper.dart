7f0951dfd4013326df20ac542007fe1cea4e7b7d
Brian Yang <yang1839@purdue.edu>
2021-04-20 20:42:25 -0400

    clean up mobile http requests

--- mobile_frontend/together_trek/lib/api/UserWrapper.dart
+++ mobile_frontend/together_trek/lib/api/UserWrapper.dart
@@ -15,44 +15,44 @@ Future<int> createUser(String data) async {
 }
 
 Future<UserModel> getUser(String id) async {
-  http.Response response = await httpGet('users/${id}');
+  http.Response response = await httpGet('users/$id');
 
   return UserModel.fromJson(jsonDecode(response.body));
 }
 
 Future<UserModel> updateUser(String id, String data) async {
-  http.Response response = await httpPut('users/${id}', data);
+  http.Response response = await httpPut('users/$id', data);
   return UserModel.fromJson(jsonDecode(response.body));
 }
 
 Future<int> deleteUser(String id) async {
-  http.Response response = await httpDelete('users/${id}');
+  http.Response response = await httpDelete('users/$id');
 
   return response.statusCode;
 }
 
 Future<int> sendFriendRequest(String id, String data) async {
-  http.Response response = await httpPost('users/request-friend/${id}', data);
+  http.Response response = await httpPost('users/request-friend/$id', data);
   return response.statusCode;
 }
 
 Future<int> acceptFriendRequest(String id, String data) async {
-  http.Response response = await httpPost('users/accept-friend/${id}', data);
+  http.Response response = await httpPost('users/accept-friend/$id', data);
   return response.statusCode;
 }
 
 Future<int> declineFriendRequest(String id, String data) async {
-  http.Response response = await httpPost('users/decline-friend/${id}', data);
+  http.Response response = await httpPost('users/decline-friend/$id', data);
   return response.statusCode;
 }
 
 Future<int> blockUser(String id, String data) async {
-  http.Response response = await httpPost('users/block-user/${id}', data);
+  http.Response response = await httpPost('users/block-user/$id', data);
   return response.statusCode;
 }
 
 Future<int> unblockUser(String id, String data) async {
-  http.Response response = await httpPost('users/unblock-user/${id}', data);
+  http.Response response = await httpPost('users/unblock-user/$id', data);
   return response.statusCode;
 }
 
@@ -71,5 +71,5 @@ Future<int> userLogin(String data) async {
 }
 
 NetworkImage getProfilePic(String id) {
-  return getNetworkImage('users/profile-pic/${id}');
+  return getNetworkImage('users/profile-pic/$id');
 }
