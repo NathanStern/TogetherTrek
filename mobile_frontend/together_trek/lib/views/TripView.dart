12ccb0d48dcc1b627a2c7386533d0b8f82b29ec8
justinhay14 <54778974+justinhay14@users.noreply.github.com>
2021-04-20 20:19:37 -0400

    added button

--- mobile_frontend/together_trek/lib/views/TripView.dart
+++ mobile_frontend/together_trek/lib/views/TripView.dart
@@ -20,7 +20,7 @@ class TripView extends StatefulWidget {
 class _TripViewState extends State<TripView> {
   _TripViewState({this.trip});
   TripModel trip;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,72 +30,74 @@ class _TripViewState extends State<TripView> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-         Text(
-                    "Start Date:",
-                    style: TextStyle(
-                        color: Colors.redAccent,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 28.0
-                        ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    trip.startDate.substring(0,10),
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                  Text(
-                    "End Date:",
-                    style: TextStyle(
-                        color: Colors.redAccent,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 28.0
-                        ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    trip.endDate.substring(0,10),
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                  Text(
-                    "Destination:",
-                    style: TextStyle(
-                        color: Colors.redAccent,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 28.0
-                        ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    trip.destination.toString(),
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                  ],
-                  ),
-                  );
+          Text(
+            "Start Date:",
+            style: TextStyle(
+                color: Colors.redAccent,
+                fontStyle: FontStyle.normal,
+                fontSize: 28.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            trip.startDate.substring(0, 10),
+            style: TextStyle(
+              fontSize: 22.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w300,
+              color: Colors.black,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            "End Date:",
+            style: TextStyle(
+                color: Colors.redAccent,
+                fontStyle: FontStyle.normal,
+                fontSize: 28.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            trip.endDate.substring(0, 10),
+            style: TextStyle(
+              fontSize: 22.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w300,
+              color: Colors.black,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            "Destination:",
+            style: TextStyle(
+                color: Colors.redAccent,
+                fontStyle: FontStyle.normal,
+                fontSize: 28.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            trip.destination.toString(),
+            style: TextStyle(
+              fontSize: 22.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w300,
+              color: Colors.black,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () async {},
+                child: Text('Request to join'),
+              ))
+        ],
+      ),
+    );
   }
 }
-
