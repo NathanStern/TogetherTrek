3f781d3d769d878b21b16b1315d6ebd38cecf2ca
Brian Yang <yang1839@purdue.edu>
2021-04-22 06:27:34 -0400

    notification page mobile

--- mobile_frontend/together_trek/lib/views/NotificationView.dart
+++ mobile_frontend/together_trek/lib/views/NotificationView.dart
@@ -1,32 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:together_trek/models/UserModel.dart';
 import 'package:provider/provider.dart';
+import 'package:together_trek/api/UserWrapper.dart';
 
 class NotificationView extends StatefulWidget {
-  _NotificationViewState createState() => _NotificationViewState();
+  NotificationView({Key key, this.user}) : super(key: key);
+  UserModel user;
+
+  _NotificationViewState createState() => _NotificationViewState(user: user);
 }
 
 class _NotificationViewState extends State<NotificationView> {
+  _NotificationViewState({this.user});
   UserModel user;
   @override
   Widget build(BuildContext context) {
-    user = context.watch<UserModel>();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text('Notifications', style: TextStyle(color: Colors.deepOrangeAccent)),
-        ),
-        body: ListView.builder(
-          itemCount: user.friendIds.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${user.friendIds[index]}'),
-            );
-          },
-        ),
+    if (user == null) {
+      return Scaffold(
+          appBar: AppBar(
+        title: Text("Friend Request"),
       ),
-    );
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "No Friend Requests",
+              style: TextStyle(
+                fontSize: 22.0,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w300,
+                color: Colors.black,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ],
+        ));
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Friend Request From"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              user.username,
+              style: TextStyle(
+                fontSize: 22.0,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w300,
+                color: Colors.black,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
