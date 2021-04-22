c63457ba8cbd57413c0185fb6f6f66c613af2378
ryangamble <15951175+ryangamble@users.noreply.github.com>
2021-04-21 20:34:21 -0400

    Added missing commas

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -1060,8 +1060,8 @@ exports.getNearbyUsers = (req, res) => {
 					'_id': user_data['_id'],
 					'username': user_data['username'],
 					'gender': user_data['gender'],
-					'birthdate': user_data['birthdate']
-					'friend_requests': user_data['friend_requests']
+					'birthdate': user_data['birthdate'],
+					'friend_requests': user_data['friend_requests'],
 					'profile_pic': user_data['profile_pic']
 				}
 				matching_users.push(user_data);
