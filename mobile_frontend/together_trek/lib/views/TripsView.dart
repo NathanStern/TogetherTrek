7c84f1974898de7a1c26a9eb865257bc6e15cd3d
Brian Yang <yang1839@purdue.edu>
2021-04-23 00:14:51 -0400

    trips page search bar

--- mobile_frontend/together_trek/lib/views/TripsView.dart
+++ mobile_frontend/together_trek/lib/views/TripsView.dart
@@ -9,6 +9,7 @@ import 'package:together_trek/utils/DialogUtil.dart';
 import 'package:together_trek/views/TripView.dart';
 
 class TripsView extends StatefulWidget {
+  @override
   _TripsViewState createState() => _TripsViewState();
 }
 
@@ -25,65 +26,140 @@ class _TripsViewState extends State<TripsView> {
     trips = context.watch<LoadedTripsModel>();
     UserModel user = context.read<UserModel>();
     int _toReverse = 1;
-    return RefreshIndicator(
-      child: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: trips.trips.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(3)),
-                elevation: 2,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    InkWell(
-                        borderRadius: BorderRadius.circular(2.5),
-                        enableFeedback: true,
-                        splashColor: Colors.deepOrangeAccent,
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      TripView(trip: trips.trips[index])));
-                        },
-                        child: ListTile(
-                            title: Text(
-                                trips.trips[index].destination.toString()))),
-                  ],
-                ));
-          }),
-      onRefresh: () async {
-        List<TripModel> retrievedTrips =
-            await getTrips().timeout(Duration(seconds: 15), onTimeout: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return buildStandardDialog(context, "Network Error",
-                    "There was an error retrieving trips from the server.");
-              });
-          _toReverse = 0;
-          return this.trips.trips;
-        }).catchError((err) {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return buildStandardDialog(
-                    context, "Network Error", err.toString());
-              });
-          _toReverse = 0;
-          return this.trips.trips;
-        });
-        setState(() {
-          if (_toReverse == 1) {
-            _saveTrips(retrievedTrips);
-          } else {
-            _toReverse = 1;
-          }
-        });
-        user.tripIds = await getTripsById(trips.trips, user.id);
+    return Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                showSearch(
+                  context: context, 
+                  delegate: SearchData(user.tripIds));
+              },
+              icon: Icon(Icons.search),
+            )
+          ],
+          centerTitle: true,
+          title: Text('Search'),
+        ),
+        body: RefreshIndicator(
+          child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: trips.trips.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(3)),
+                    elevation: 2,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                            borderRadius: BorderRadius.circular(2.5),
+                            enableFeedback: true,
+                            splashColor: Colors.deepOrangeAccent,
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          TripView(trip: trips.trips[index])));
+                            },
+                            child: ListTile(
+                                title: Text(trips.trips[index].destination
+                                    .toString()))),
+                      ],
+                    ));
+              }),
+          onRefresh: () async {
+            List<TripModel> retrievedTrips =
+                await getTrips().timeout(Duration(seconds: 15), onTimeout: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return buildStandardDialog(context, "Network Error",
+                        "There was an error retrieving trips from the server.");
+                  });
+              _toReverse = 0;
+              return this.trips.trips;
+            }).catchError((err) {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return buildStandardDialog(
+                        context, "Network Error", err.toString());
+                  });
+              _toReverse = 0;
+              return this.trips.trips;
+            });
+            setState(() {
+              if (_toReverse == 1) {
+                _saveTrips(retrievedTrips);
+              } else {
+                _toReverse = 1;
+              }
+            });
+            user.tripIds = await getTripsById(trips.trips, user.id);
+          },
+        )
+        );
+        
+  }
+}
+
+class SearchData extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  String selectedResult;
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text(selectedResult),
+    ));
+  }
+
+  final List<String> list;
+  SearchData(this.list);
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestionList = [];
+    query.isEmpty
+        ? suggestionList = list
+        : suggestionList.addAll(list.where(
+            (element) => element.contains(query),
+          ));
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+            title: Text(
+              suggestionList[index],
+            ),
+            onTap: () {
+              selectedResult = suggestionList[index];
+              showResults(context);
+            });
       },
     );
   }
